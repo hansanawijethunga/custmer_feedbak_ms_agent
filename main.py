78368c8d1ad28bc37bac0eef82c09c5aa49f6a19
hansanawijethunga <hansanacwijethunga@gmail.com>
2025-03-11 13:32:36 +0530

    wrote data on google sheet

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from typing import List
+import uuid
+from datetime import datetime
+
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = "1RDobTxzcJB5RBtKf1BibfBNTKV0W4anu3_jsYe7K00I"
+
 
 # Set your OpenAI API key
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # Replace with your actual API key
@@ -35,7 +40,10 @@ class QueryRequest(BaseModel):
     recommendation: str
     stay_again: bool
     review: str
-
+    name : str
+    age : str
+    companion : List[str]
+    referralSource : str
 
 @app.route("/feedback", methods=["POST"])
 def chat_with_ai():
@@ -44,21 +52,23 @@ def chat_with_ai():
         query = QueryRequest(**request_data)
         print(query)
         client = OpenAI()
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": prompt.PROMPT},
-                {
-                    "role": "user",
-                    "content": query.review
-                }
-            ],
-            response_format={
-                "type": "json_object"
-            },
-        )
-        # print(response)
-        data = json.loads(response.choices[0].message.content)
+        # response = client.chat.completions.create(
+        #     model="gpt-4o-mini",
+        #     messages=[
+        #         {"role": "system", "content": prompt.PROMPT},
+        #         {
+        #             "role": "user",
+        #             "content": query.review
+        #         }
+        #     ],
+        #     response_format={
+        #         "type": "json_object"
+        #     },
+        # )
+        #
+        #
+        # data = json.loads(response.choices[0].message.content)
+        data = []
         print(type(data))
         creds = None
         if os.path.exists("token.json"):
@@ -76,11 +86,43 @@ def chat_with_ai():
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
 
-            try:
-                service = build("sheets", "v4", credentials=creds)
-                sheet = service.spreadsheets()
-            except:
-                pass
+        try:
+            feedback_sheet_range = 'FeedBack!A1'
+            service = build("sheets", "v4", credentials=creds)
+            unique_id = uuid.uuid4()
+            print(unique_id)
+            # sheet = service.spreadsheets()
+            row = [
+                str(unique_id),
+                datetime.now().strftime("%Y"),
+                datetime.now().strftime("%m"),
+                query.overall_experience,
+                query.room_rating,
+                query.customer_service,
+                query.dining_experience,
+                query.food_variety,
+                query.parking_convenience,
+                query.location_accessibility,
+                query.recommendation,
+                query.stay_again,
+                query.review,
+                query.name,
+                query.age,
+                query.referralSource
+                ]
+            command = service.spreadsheets().values().append(
+                spreadsheetId=SPREADSHEET_ID,
+                range=feedback_sheet_range,
+                valueInputOption="RAW",  # Use "RAW" to input raw data, "USER_ENTERED" for Excel-like behavior
+                insertDataOption="INSERT_ROWS",  # Optionally choose how to insert rows
+                body={"values": [row]}
+            )
+
+            # Execute the request
+            response = command.execute()
+            print(response)
+        except Exception as e:
+            print(e)
 
         return jsonify(data)
 
